75dada5711096c33eb65f8481785c7e43a62650c
tomercarolla <carollatomer@gmail.com>
2025-07-26 18:25:48 +0300

    configs

--- eslint.config.js
+++ eslint.config.js
@@ -4,11 +4,15 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import unusedImports from 'eslint-plugin-unused-imports';
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'unused-imports': unusedImports,
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
